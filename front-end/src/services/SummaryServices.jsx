f5c3b77fa99cd6b210a40fd0aad4ddf511453a50
Sasikumar <mr.inspiring5566@gmail.com>
2025-01-23 21:12:48 +0530

    Update files

--- front-end/src/services/SummaryServices.jsx
+++ front-end/src/services/SummaryServices.jsx
@@ -1,18 +1,17 @@
 import axios from 'axios'
-const baseUrl =  'https://expense-tracker-three-wheat.vercel.app'
 
 export const addUser = async(name) => {
-    return axios.post(`${baseUrl}/add/summaryuser`, {user: name})
+    return axios.post(`${import.meta.env.VITE_BASEURL}/add/summaryuser`, {user: name})
 }
 
 export const fetchAllUsers = async() => {
-    return axios.get(`${baseUrl}/get/alluser`)
+    return axios.get(`${import.meta.env.VITE_BASEURL}/get/alluser`)
 }
 
 export const addDetails = async(details) => {
-    return axios.post(`${baseUrl}/add/details`, details)
+    return axios.post(`${import.meta.env.VITE_BASEURL}/add/details`, details)
 }
 
 export const getIndividualDetails = async(id) => {
-    return axios.get(`${baseUrl}/get/details`,{params: {user_id: id}})
+    return axios.get(`${import.meta.env.VITE_BASEURL}/get/details`,{params: {user_id: id}})
 }
